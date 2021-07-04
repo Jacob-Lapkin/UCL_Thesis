b92e2c67868cde996caa818ab6e058af89f60ad8
Post Shalom <jacobglapkin@gmail.com>
2021-07-04 15:50:08 +0100

    extracting frames as images from video

--- pose/AITrainer.py
+++ pose/AITrainer.py
@@ -8,6 +8,7 @@ import PoseModule as pm
 def ShotAngle(path, stroke, showall, angle_type):
     cap = cv2.VideoCapture(path)
     detector = pm.poseDetector()
+    #empty = []
     while True:
         success, img = cap.read()
         # showall: False shows just points in function while True shows all points on body
@@ -35,6 +36,18 @@ def ShotAngle(path, stroke, showall, angle_type):
                 angle_right_armpit = detector.findAngle(img, 13, 11, 23)
                 angle_left_armpit = detector.findAngle(img, 14, 12, 24)
 
+            elif angle_type == 0:
+                # all angles
+                angle_right_leg = detector.findAngle(img, 24, 26, 28)
+                angle_left_leg = detector.findAngle(img, 23, 25, 27)
+                angle_right_arm = detector.findAngle(img, 12, 14, 16)
+                angle_left_arm = detector.findAngle(img, 11, 13, 15)
+                angle_right_body = detector.findAngle(img, 12, 24, 26)
+                angle_left_body = detector.findAngle(img, 11, 23, 25)
+                angle_right_armpit = detector.findAngle(img, 13, 11, 23)
+                angle_left_armpit = detector.findAngle(img, 14, 12, 24)
+                
+
             # Draw  Count
             # X cordinate 
             shape = img.shape
@@ -49,9 +62,10 @@ def ShotAngle(path, stroke, showall, angle_type):
             cv2.putText(img, 'Test', (int(alignX), int(alignY)), cv2.FONT_HERSHEY_PLAIN, 2,
                         (0, 255, 255), 2)    
 
-        #print(someangle)            
+   
+        #print(angle_right_leg, angle_left_leg)          
         cv2.imshow("Image", img)
         cv2.waitKey(1)
-
-ShotAngle('videos/serve/nadal/nadalserveside.mp4', 'serve', False, 1)
+    
+ShotAngle('videos/serve/nadal/nadalserveside.mp4', 'serve', False, 0)
 
