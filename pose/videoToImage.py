fe69fa612e018011fc2f1f2da33b14ad805fcbdd
Post Shalom <jacobglapkin@gmail.com>
2021-07-04 15:53:27 +0100

    function for convertion

--- pose/videoToImage.py
+++ pose/videoToImage.py
@@ -1,14 +1,17 @@
 import cv2
  
 # Opens the Video file
-cap= cv2.VideoCapture('videos/serve/nadal/nadalserveside.mp4')
-i=0
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    if ret == False:
-        break
-    cv2.imwrite('kang'+str(i)+'.jpg',frame)
-    i+=1
- 
-cap.release()
-cv2.destroyAllWindows()
+def convertVideo(path):
+    cap= cv2.VideoCapture(path)
+    i=0
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == False:
+            break
+        cv2.imwrite('kang'+str(i)+'.jpg',frame)
+        i+=1
+    
+    cap.release()
+    cv2.destroyAllWindows()
+
+convert('videos/serve/nadal/nadalserveside.mp4')
