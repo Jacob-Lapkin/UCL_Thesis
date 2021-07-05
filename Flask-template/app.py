765664d8160ddb11bef4c4172562d72ff8b74a99
Post Shalom <jacobglapkin@gmail.com>
2021-07-05 16:27:36 +0100

    added dashboard html file and outline for it

--- Flask-template/app.py
+++ Flask-template/app.py
@@ -12,6 +12,10 @@ from wtforms import (Form, StringField, TextField, SubmitField, PasswordField,
 from wtforms.validators import DataRequired, Length, Email, EqualTo 
 from wtforms import ValidationError
 
+# importing pose data
+from pose.angledata import display_df
+
+
 
 #####################
 ###### CONFIRG ######
@@ -58,7 +62,7 @@ class point(FlaskForm):
     description = TextField("Description", validators=[DataRequired()])
     point_type = RadioField('Point Type', validators=[DataRequired()], choices=[('choice1', 'practice'), ('choice2', 'match')])
     point_upload = FileField('Upload', validators=[DataRequired()])
-    submit = SubmitField('Register')
+    submit = SubmitField('Submit')
 
 class Stroke(FlaskForm):
     date = DateField('Date', validators=[DataRequired()])
@@ -66,7 +70,7 @@ class Stroke(FlaskForm):
     stroke_type = SelectField('Stroke Type', validators=[DataRequired()], choices = [('choice1', 'Serve'), ('choice2', 'Forehand return'), ('choice3', 'backhand return'), ('choice4', 'Volley')])
     pro_comparison = SelectField('Professional Comparison', choices = [('choice1', 'Novak Djokavic'), ('choice2', 'Roger Federer'), ('choice3', 'Rafael Nadal'), ('choice4', 'Serena Williams')], validators = [DataRequired()])
     stroke_upload = FileField('Upload', validators=[DataRequired()])
-    submit = SubmitField('Register')
+    submit = SubmitField('Submit')
 
 #################################
 ######## MODELS #################
@@ -140,6 +144,12 @@ def stroke():
     if form.validate_on_submit():
         return redirect(url_for('login'))
     return render_template('stroke.html', form=form)
+
+@app.route('/results')
+def results():
+    return render_template('graphs.html')
+
+
 
 
 @app.errorhandler(404)
